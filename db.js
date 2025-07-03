141f4f7643b7756bbce726be9c502e5ab63c37d9
Abhilashmahi <abikavirahul1234@gmail.com>
2025-07-03 15:50:16 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',     // default for XAMPP
-  password: '',     // default is blank in XAMPP
-  database: 'quickmeet'
+  host: 'sql12.freesqldatabase.com',
+  user: 'sql12788087',
+  password: 'GH26sp92Ip',
+  database: 'sql12788087',
+  port: 3306
 });
 
 db.connect((err) => {
@@ -12,7 +13,7 @@ db.connect((err) => {
     console.error('DB connection failed:', err.stack);
     return;
   }
-  console.log('MySQL connected');
+  console.log('MySQL connected to FreeSQLDatabase.com');
 });
 
 module.exports = db;
